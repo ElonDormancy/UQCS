eecfc7caa08c5fbe04a07056775493c8cddec21f
Dormancy <60100807+ElonDormancy@users.noreply.github.com>
2021-11-24 19:23:00 +0800

    Delete QSC++OMP directory

--- QSC++OMP/SGO.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SGOperator
-#define _SGOperator
-#include "datatypes.h"
-#include <omp.h>
-
-VectorC SGateOperator(VectorC vec_state, u_int index, gate Gate);
-
-#endif
--- QSC++OMP/init.h
+++
@@ -1,9 +0,0 @@
-#ifndef _INIT
-#define _INIT
-#include "datatypes.h"
-#include <Eigen\Dense>
-using namespace Eigen;
-
-VectorC init_vector(VectorXd psi);
-VectorXd  init_psi(u_int n);
-#endif
